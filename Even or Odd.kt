c0831f2e1335c4b811b245a619df43fdeeb558b4
Advik G M <vaishnavi.dec04@gmail.com>
2026-03-19 12:36:17 +0530

    Even or Odd.kt

---
+++ Even or Odd.kt
@@ -0,0 +1,16 @@
+
+fun main() {
+    // 1. Pick a number
+    val myNumber = 15 
+
+    // 2. Check the remainder when divided by 2
+    if (myNumber % 2 == 0) {
+        println("$myNumber is Even")
+    } else {
+        println("$myNumber is Odd")
+    }
+}
+
+
+
+

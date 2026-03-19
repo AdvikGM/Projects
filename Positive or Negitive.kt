cbd34f4ecf29c9b6b51a5ca3cc8d73de699bee95
Advik G M <vaishnavi.dec04@gmail.com>
2026-03-19 13:33:34 +0530

    Positive or Negitive.kt

---
+++ Positive or Negitive.kt
@@ -0,0 +1,8 @@
+var num = 19
+when {
+  num < 0 -> println("$num is negative.")
+  num == 0 -> println("$num is zero.")
+  num > 0 -> println("$num is positive.")
+  else -> println("Not a valid number.")
+}
+

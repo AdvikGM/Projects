f197a708e4b151de49d0bcd9cf7bcfa03b66899f
Advik G M <vaishnavi.dec04@gmail.com>
2026-03-22 14:36:02 +0530

    NumberLessthanoneormor.kt

---
+++ NumberLessthanoneormor.kt
@@ -0,0 +1,13 @@
+fun main() { 
+    var num = 30 
+    
+    if (num > 20) { 
+        println("This number is greater than 20") 
+    }  
+    if (num < 20)  { 
+        println("This number is lesser than 20") 
+    } 
+    if (num == 20) { 
+        println("This number is equal to 20") 
+    }
+    ]

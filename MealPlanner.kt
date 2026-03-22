01f7e24764a4f435ec91c82a726c8d34ea5a77ca
Advik G M <vaishnavi.dec04@gmail.com>
2026-03-22 14:39:38 +0530

    MealPlanner.kt

---
+++ MealPlanner.kt
@@ -0,0 +1,19 @@
+
+var mealMonday = Dosa
+var mealTuesday = Idli
+var mealWednesday = Rice
+var mealThursday = Chapati
+var mealFriday = Poori
+       println("Enter day:")
+var day = readln()
+if (day = mealMonday) {
+println("Make dosa")  }
+if (day = mealTuesday) {
+println("Make idli")  }
+if (day = mealWednesday) {
+println("Make rice")  }
+if (day = mealThursday) {
+println("Make chapati")  }
+if (day = mealFriday) {
+println("Make poori")  }
+else {println("Make whatever you want")
